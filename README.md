6343e0b8aed02f189d7b28255b2621c91bbdf10f
SsiCipher <ssicipher@mail.com>
2022-04-02 05:51:40 +0000

    starting over

--- README.md
+++ README.md
@@ -13,4 +13,11 @@ print_stacks(stack_a, stack_b);
 start_interactive_tester(stack_a, stack_b);
 // ----- TESTING ONLY
 
+// ----- Printa array ONLY
+int i = -1;
+while (new_argv[++i])
+	printf("argv[%d] = [%s]\n", i, new_argv[i]);
+printf("argc = [%d]\n", new_argc);
+// ----- Printa array ONLY
+
 ```
--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: yanab <yanab@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 21:07:56 by yanab             #+#    #+#             */
-/*   Updated: 2022/04/02 05:28:55 by yanab            ###   ########.fr       */
+/*   Updated: 2022/04/02 05:43:38 by yanab            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,30 +49,62 @@ bool	contains_dups(int argc, char **argv)
 	while (--argc)
 	{
 		i = -1;
-		while (i < argc)
+		while (++i < argc)
 		{
-			if (ft_strcmp(argv[argc], argv[i]) == 0)
+			if (!ft_strcmp(argv[argc], argv[i]))
 				return (TRUE);
 		}
 	}
 	return (FALSE);
 }
 
-void	init_stack(t_node **stack_top, int argc, char **argv)
+int	atoi_check(char *number)
 {
-	int		i;
+	int			i;
+	int			sign;
+	long long	num;
+
+	i = 0;
+	sign = 1;
+	if (number[i] == '-' || number[i] == '+')
+		if (number[i++] == '-')
+			sign *= -1;
+	num = 0;
+	while (number[i])
+	{
+		if (number[i] < '0' || number[i] > '9')
+			print_error("Error: the arguments contain a non integer");
+		num = num * 10 + (number[i++] - '0');
+	}
+	if (sign * num <= INT_MIN || sign * num >= INT_MAX)
+		print_error("Error: the arguments are out of integer range");
+	return (sign * num);
+}
+
+void	init_stack(t_stack *stack, int argc, char **argv)
+{
+	int		n;
 	int		new_argc;
 	char	**new_argv;
+	t_node	*new_node;
 
-	(void)stack_top;
+	(void)stack;
 	new_argc = 0;
 	new_argv = NULL;
 	parse_args(argc, argv, &new_argc, &new_argv);
+
 	if (contains_dups(new_argc, new_argv))
 		print_error("Error:\nThe arguments contain duplicates");
-	// check stack for duplicates
-	// check stack for non-integers
-	// check stack for out of INT range	
+	while (new_argc--)
+	{
+		n = atoi_check(new_argv[new_argc]);
+		new_node = (t_node *)malloc(sizeof(t_node));
+		if (!new_node)
+			print_error("Error:\nfailed to allocate memory");
+		new_node->content = n;
+		new_node->next = NULL;
+		// push to stack
+	}
 }
 
 int main(int argc, char **argv)
@@ -81,7 +113,7 @@ int main(int argc, char **argv)
 	// t_stack stack_b;
 
 	// parse args & init stacks
-	init_stack(&(stack_a.stack_top), argc - 1, argv + 1);
+	init_stack(&stack_a, argc - 1, argv + 1);
 
 	// index stack
 	// sort stack
